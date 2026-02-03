7ab7c24347871a925e6f824b9aa5f9f1ea4d8cc7
arundaya <runsworkspace@gmail.com>
2026-02-04 02:02:36 +0700

    fix: fixed false skeleton column count

--- components/canteen/canteen-list-skeleton.tsx
+++ components/canteen/canteen-list-skeleton.tsx
@@ -19,7 +19,7 @@ export function CanteenListSkeleton({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6",
+        "grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6",
         className,
       )}
     >
