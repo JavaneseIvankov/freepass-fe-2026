c8fd1faa30f56387babf8c68e9dc9b660a2efcae
arundaya <runsworkspace@gmail.com>
2026-02-04 04:56:58 +0700

    feat: implemented conditional navbar, only displays navbar on exact route match

--- app/(customer)/layout.tsx
+++ app/(customer)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/navbar";
+import ConditionalNavbar from "@/components/conditional-navbar";
 
 export default function RootLayout({
   children,
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <>
       <main className="pb-10">{children}</main>
-      <Navbar />
+      <ConditionalNavbar />
     </>
   );
 }
